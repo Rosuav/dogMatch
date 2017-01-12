e6bf096d78242e266523c1f8621a23e7447b2364
Donna Oshiro <oshirodk@gmail.com>
2017-01-12 07:44:03 -0800

    broken code

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -13,18 +13,6 @@ const Answers = React.createClass ({
 		});
 	},
 
-	handleFormSubmit(formSubmitEvent) {
-		formSubmitEvent.preventDefault();
-
-		console.log('You have selected : ', this.state.selectedOption)
-		const results = {
-			question1: this.question1.value,
-			question2: this.question2.value,
-			question3: this.question3.value
-		}
-		console.log(results);
-	},
-
 	render() {
 		console.log(this.props, "DA PROPS"); 
 		for (var i = 0; i > this.props.quiz.answers.length; i++) {
@@ -36,7 +24,7 @@ const Answers = React.createClass ({
 			<form>
 				<div className="radio">
 					<label>
-						<input ref={(input) => this.question1 = input} type="radio" value="option1" 
+						<input ref={(input) => this.option1 = input} type="radio" value="option1" 
 						checked={this.state.selectedOption === 'option1'} 
 						onChange={this.handleOptionChange}/>
 						{this.props.quiz.answers[0]}
@@ -44,7 +32,7 @@ const Answers = React.createClass ({
 				</div>
 				<div className="radio">
 					<label>
-						<input ref={(input) => this.question1 = input} type="radio" value="option2" 
+						<input ref={(input) => this.option2 = input} type="radio" value="option2" 
 						checked={this.state.selectedOption === 'option2'} 
 						onChange={this.handleOptionChange}/>
 						{this.props.quiz.answers[1]}
@@ -52,7 +40,7 @@ const Answers = React.createClass ({
 				</div>
 				<div className="radio">
 					<label>
-						<input ref={(input) => this.question1 = input} type="radio" value="option3" 
+						<input ref={(input) => this.option3 = input} type="radio" value="option3" 
 						checked={this.state.selectedOption === 'option3'} 
 						onChange={this.handleOptionChange}/>
 						{this.props.quiz.answers[2]}
--- src/components/QuestionPreview.js
+++ src/components/QuestionPreview.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Answers from './Answers';
 
-const QuestionPreview = (quiz) => (
-	<div className="QuestionPreview">
-		<div className="questions">
-			{quiz.questions}
-		</div>
-		<div className="answers">
-			<Answers quiz={quiz} />
-		</div>
-	</div>
-);
+
+class QuestionPreview extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleFormSubmit.bind(this)
+	}
+
+	handleFormSubmit(formSubmitEvent) {
+		formSubmitEvent.preventDefault();
+
+		console.log('You have selected : ', this.state.selectedOption)
+		const results = {
+			option1: this.question1.value,
+			option2: this.question2.value,
+			option3: this.question3.value
+		}
+		console.log(results);
+		console.log(this.props.quiz.questions, "QIZZ")
+	}
+	
+
+	render() {
+		return (
+			<div className="QuestionPreview">
+				<div className="questions">
+				{quiz.questions}
+				</div>
+				<form className="answers" onSubmit={this.handleFormSubmit}>
+					<Answers quiz={this.props.quiz} />
+					<button className="btn btn-default" type="submit">Results</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+
 
 export default QuestionPreview;
