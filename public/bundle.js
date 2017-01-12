ec477f2c84d3c607b06deb0ffa306fe30bae51df
Donna Oshiro <oshirodk@gmail.com>
2017-01-11 16:20:24 -0800

    adds a onsubmit function in answers component

--- public/bundle.js
+++ public/bundle.js
@@ -21725,20 +21725,27 @@
 		},
 		handleOptionChange: function handleOptionChange(changeEvent) {
 			this.setState({
-				selectedOption: changeEvent.target.value
+				selectedOption: 'changeEvent.target.value'
 			});
+		},
+		handleFormSubmit: function handleFormSubmit(formSubmitEvent) {
+			formSubmitEvent.preventDefault();
+
+			console.log('You have selected : ', this.state.selectedOption);
 		},
 		render: function render() {
 			return _react2.default.createElement(
 				'form',
-				{ className: 'answers' },
+				{ onSubmit: this.handleFormSubmit },
 				_react2.default.createElement(
 					'div',
 					{ className: 'radio' },
 					_react2.default.createElement(
 						'label',
 						null,
-						_react2.default.createElement('input', { type: 'radio', value: 'option1', checked: this.state.selectedOption === 'option1', onChange: this.handleOptionChange }),
+						_react2.default.createElement('input', { type: 'radio', value: 'option1',
+							checked: this.state.selectedOption === 'option1',
+							onChange: this.handleOptionChange }),
 						'Option 1'
 					)
 				),
@@ -21748,7 +21755,9 @@
 					_react2.default.createElement(
 						'label',
 						null,
-						_react2.default.createElement('input', { type: 'radio', value: 'option1', checked: this.state.selectedOption === 'option1', onChange: this.handleOptionChange }),
+						_react2.default.createElement('input', { type: 'radio', value: 'option2',
+							checked: this.state.selectedOption === 'option1',
+							onChange: this.handleOptionChange }),
 						'Option 2'
 					)
 				),
@@ -21758,7 +21767,9 @@
 					_react2.default.createElement(
 						'label',
 						null,
-						_react2.default.createElement('input', { type: 'radio', value: 'option1', checked: this.state.selectedOption === 'option1', onChange: this.handleOptionChange }),
+						_react2.default.createElement('input', { type: 'radio', value: 'option3',
+							checked: this.state.selectedOption === 'option1',
+							onChange: this.handleOptionChange }),
 						'Option 3'
 					)
 				),
--- src/components/Answers.js
+++ src/components/Answers.js
@@ -4,33 +4,45 @@ const Answers = React.createClass ({
 	getInitialState() {
 		return {
 			selectedOption: 'option1'
-		}
+		};
 	},
 
 	handleOptionChange(changeEvent) {
 		this.setState({
-			selectedOption: changeEvent.target.value
-		})
+			selectedOption: 'changeEvent.target.value'
+		});
+	},
+
+	handleFormSubmit(formSubmitEvent) {
+		formSubmitEvent.preventDefault();
+
+		console.log('You have selected : ', this.state.selectedOption)
 	},
 
 	render() {
 		return (
-			<form className="answers">
+			<form onSubmit={this.handleFormSubmit}>
 				<div className="radio">
 					<label>
-						<input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange}/>
+						<input type="radio" value="option1" 
+						checked={this.state.selectedOption === 'option1'} 
+						onChange={this.handleOptionChange}/>
 						Option 1
 					</label>
 				</div>
 				<div className="radio">
 					<label>
-						<input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange}/>
+						<input type="radio" value="option2" 
+						checked={this.state.selectedOption === 'option1'} 
+						onChange={this.handleOptionChange}/>
 						Option 2
 					</label>
 				</div>
 				<div className="radio">
 					<label>
-						<input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange}/>
+						<input type="radio" value="option3" 
+						checked={this.state.selectedOption === 'option1'} 
+						onChange={this.handleOptionChange}/>
 						Option 3
 					</label>
 				</div>
